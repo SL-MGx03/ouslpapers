2803ff77037a8fe6405f9ef8623bc351b96995e0
NGMG ᴹᴳˣ03 <78734430+SL-MGx03@users.noreply.github.com>
2025-05-20 14:47:37 +0530

    Create courses.js

---
+++ courses.js
@@ -0,0 +1,54 @@
+const coursesData = [
+  {
+    subject: "Pure Maths",
+    courses: [
+      { code: "PEU3300", cat: "#", final: "#" },
+      { code: "PEU3301", cat: "#", final: "#" },
+      { code: "PEU3202", cat: "#", final: "#" }
+    ]
+  },
+  {
+    subject: "Applied Maths",
+    courses: [
+      { code: "ADU3300", cat: "#", final: "#" },
+      { code: "ADU3201", cat: "#", final: "#" },
+      { code: "ADU3302", cat: "#", final: "#" }
+    ]
+  },
+  {
+    subject: "Computer Science",
+    courses: [
+      { code: "CSU3200", cat: "#", final: "#" },
+      { code: "CSU3301", cat: "#", final: "#" }
+    ]
+  },
+  {
+    subject: "Chemistry",
+    courses: [
+      { code: "CYU3300", cat: "#", final: "#" },
+      { code: "CYU3201", cat: "#", final: "#" },
+      { code: "CYU3302", cat: "#", final: "#" }
+    ]
+  },
+  {
+    subject: "Physics",
+    courses: [
+      { code: "PHU3300", cat: "#", final: "#" },
+      { code: "PHU3301", cat: "#", final: "#" }
+    ]
+  },
+  {
+    subject: "Zoology",
+    courses: [
+      { code: "ZYU3500", cat: "#", final: "#" },
+      { code: "ZYU3301", cat: "#", final: "#" }
+    ]
+  },
+  {
+    subject: "Botany",
+    courses: [
+      { code: "BYU3500", cat: "#", final: "#" },
+      { code: "BYU3301", cat: "#", final: "#" }
+    ]
+  }
+];
