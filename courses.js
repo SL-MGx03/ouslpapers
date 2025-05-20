4c6f149b2c37d3eded004d21537800792280e3e7
NGMG ᴹᴳˣ03 <78734430+SL-MGx03@users.noreply.github.com>
2025-05-20 18:08:49 +0530

    Update courses.js

--- courses.js
+++ courses.js
@@ -2,24 +2,25 @@ const coursesData = [
   {
     subject: "Pure Maths",
     courses: [
-      { code: "PEU3300", cat: "https://drive.google.com/drive/folders/15zMqaHH6jzqlVhK2-rBDWXxDOoV2Thjq?usp=drive_link", final: "https://drive.google.com/drive/folders/15zMqaHH6jzqlVhK2-rBDWXxDOoV2Thjq?usp=drive_link" },
-      { code: "PEU3301", cat: "#", final: "#" },
-      { code: "PEU3202", cat: "#", final: "#" }
+      { code: "PEU3300", cat: "https://drive.google.com/drive/folders/115mP1p2GZxeVV3kD9QrFukuwccs63ns7?usp=drive_link", final: "https://drive.google.com/drive/folders/115mP1p2GZxeVV3kD9QrFukuwccs63ns7?usp=drive_link" },
+      { code: "PEU3301", cat: "https://drive.google.com/drive/folders/1bfxY5ETiSWZF1Ly2fjOj0Tj2HEvJmSq5?usp=drive_link", final: "https://drive.google.com/drive/folders/1bfxY5ETiSWZF1Ly2fjOj0Tj2HEvJmSq5?usp=drive_link" },
+      { code: "PEU3202", cat: "https://drive.google.com/drive/folders/181igDEUxO2yBE3Cj1KhL8h7_lxKnQn5E?usp=drive_link", final: "https://drive.google.com/drive/folders/181igDEUxO2yBE3Cj1KhL8h7_lxKnQn5E?usp=drive_link" }
     ]
   },
   {
     subject: "Applied Maths",
     courses: [
-      { code: "ADU3300", cat: "#", final: "#" },
-      { code: "ADU3201", cat: "#", final: "#" },
-      { code: "ADU3302", cat: "#", final: "#" }
+      { code: "ADU3300", cat: "https://drive.google.com/drive/folders/11LLJPJuKH0PzYuNirsfTAd8A_TfOPQzY?usp=drive_link", final: "https://drive.google.com/drive/folders/11LLJPJuKH0PzYuNirsfTAd8A_TfOPQzY?usp=drive_link" },
+      { code: "ADU3201", cat: "https://drive.google.com/drive/folders/1leA5tbLmEf5Scbz-2Q4tillJjqbDgXBi?usp=drive_link", final: "https://drive.google.com/drive/folders/1leA5tbLmEf5Scbz-2Q4tillJjqbDgXBi?usp=drive_link" },
+      { code: "ADU3302", cat: "https://drive.google.com/drive/folders/1u1dUzzBFjvyqzkx_sthEN1vYTE06RHLj?usp=drive_link", final: "https://drive.google.com/drive/folders/1u1dUzzBFjvyqzkx_sthEN1vYTE06RHLj?usp=drive_link" }
     ]
   },
   {
     subject: "Computer Science",
     courses: [
       { code: "CSU3200", cat: "https://drive.google.com/drive/folders/1NAkPuZizTiVhWfbeMAm2-lvEobV6qxbW?usp=drive_link", final: "https://drive.google.com/drive/folders/1Hyyh_YJ_2pVsQXxi-KEUuc6ybi_nuXYg?usp=drive_link" },
-      { code: "CSU3301", cat: "https://drive.google.com/drive/folders/1Ume8N0gyANT2Dl5IMaZbCbs19W7D_DKL?usp=drive_link", final: "https://drive.google.com/drive/folders/1WBfFh0N4_WK2uewFs19L3Nyqh8epZBRC?usp=drive_link" }
+      { code: "CSU3301", cat: "https://drive.google.com/drive/folders/1Ume8N0gyANT2Dl5IMaZbCbs19W7D_DKL?usp=drive_link", final: "https://drive.google.com/drive/folders/1WBfFh0N4_WK2uewFs19L3Nyqh8epZBRC?usp=drive_link" },
+      { code: "CSU3302", cat: "https://drive.google.com/drive/folders/1cPw28MnYfc0Emfyzjh5PrTLL5vHeRHlw?usp=drive_link", final: "https://drive.google.com/drive/folders/1cPw28MnYfc0Emfyzjh5PrTLL5vHeRHlw?usp=drive_link"}
     ]
   },
   {
